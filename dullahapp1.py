7266d9488e9b3daa40e05a7d5cd9d4dc08171549
Dullahsulaiman <91151486+Dullahsulaiman@users.noreply.github.com>
2023-09-05 12:39:02 -0500

    Update dullahapp1.py

--- dullahapp1.py
+++ dullahapp1.py
@@ -13,7 +13,7 @@ def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
         return my_cur.fetchall()
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
+        
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows = get_fruit_load_list()
+streamlit.dataframe(my_data_rows)
