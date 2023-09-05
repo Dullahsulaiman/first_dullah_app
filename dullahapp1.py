05359315e2d5fc8808e8a7e5e4fb4d7c8e78fde8
Dullahsulaiman <91151486+Dullahsulaiman@users.noreply.github.com>
2023-09-05 12:39:57 -0500

    Update dullahapp1.py

--- dullahapp1.py
+++ dullahapp1.py
@@ -9,11 +9,12 @@ my_fruit_list = pandas.read_csv(
 )
 
 streamlit.header("The fruit load list contains:")
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-        return my_cur.fetchall()
         
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_data_rows = get_fruit_load_list()
-streamlit.dataframe(my_data_rows)
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
